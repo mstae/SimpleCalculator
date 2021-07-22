959582a0b9d3a10bfb64d7589429b975d96019ef
mstae <tahwang@microsoft.com>
2021-07-22 18:09:45 +1000

    Updated unit tests to use xunit library

--- AppUnitTests/UnitTest1.cs
+++ AppUnitTests/UnitTest1.cs
@@ -1,57 +1,56 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using DemoCalculatorApp;
+using Xunit; 
 
 namespace AppUnitTests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public void TestAddition()
         {
             CalculatorLogic c1 = new CalculatorLogic();
             double expectedResult = 16;
             double actualResult = c1.SimpleAddition(8, 8);
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
 
             expectedResult = 20;
             actualResult = c1.SimpleMultiplication(5, 4);
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
         }
-        [TestMethod]
+        [Fact]
         public void TestSubtraction()
         {
             CalculatorLogic c1 = new CalculatorLogic();
             double expectedResult = 2;
-            double actualResult = c1.SimpleSubtraction(9, 1);
-            Assert.AreEqual(expectedResult, actualResult);
+            double actualResult = c1.SimpleSubtraction(9, 7);
+            Assert.Equal(expectedResult, actualResult);
         }
 
-        [TestMethod]
+        [Fact]
         public void TestMultiplication()
         {
             CalculatorLogic c1 = new CalculatorLogic();
             double expectedResult = 20;
             double actualResult = c1.SimpleMultiplication(5, 4);
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
         }
 
-        [TestMethod]
+        [Fact]
         public void TestDivision()
         {
             CalculatorLogic c1 = new CalculatorLogic();
             double expectedResult = 5;
             double actualResult = c1.SimpleDivision(20, 4);
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
         }
-        [TestMethod]
+        [Fact]
         public void TestRemainder()
         {
             CalculatorLogic c1 = new CalculatorLogic();
             double expectedResult = 2;
             double actualResult = c1.SimpleRemainder(17, 5);
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.Equal(expectedResult, actualResult);
         }
     }
 }
