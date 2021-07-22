ef54c38653941f0a615a691dc9d5ac246f5e416a
mstae <tahwang@microsoft.com>
2021-07-22 16:03:48 +1000

    Added AppCenter config and added more details around testing build

--- DemoCalculatorApp/App.xaml.cs
+++ DemoCalculatorApp/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace DemoCalculatorApp
 {
@@ -15,6 +18,10 @@ namespace DemoCalculatorApp
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=59a9819d-993f-4552-945f-a8ab06792cec;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
